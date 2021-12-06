f07f3f8d7ab8163748e9d44c6afbabeae2218d7c
Luciano <lucianoeberhardt95@outlook.com>
2021-12-06 19:21:45 -0300

    start

---
+++ app/Models/User/Profile/Link.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\User\Profile;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Link extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2021_12_06_221805_create_profiles_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('nickname')->nullable();
+            $table->string('description')->nullable();
+            $table->string('facebook')->nullable();
+            $table->string('instagram')->nullable();
+            $table->string('tiktok')->nullable();
+            $table->string('twitter')->nullable();
+            $table->string('gmaps')->nullable();
+            $table->string('whatsapp')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('phone_2')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
