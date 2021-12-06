8fe827c4905a4dd32030467695974883c75c40ab
Luciano <lucianoeberhardt95@outlook.com>
2021-12-06 19:55:33 -0300

    url do profile

--- database/migrations/2021_12_06_221805_create_profiles_table.php
+++ database/migrations/2021_12_06_221805_create_profiles_table.php
@@ -17,6 +17,7 @@ class CreateProfilesTable extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
+            $table->string('url')->unique()->nullable();
             $table->string('nickname')->nullable();
             $table->string('description')->nullable();
             $table->string('facebook')->nullable();
