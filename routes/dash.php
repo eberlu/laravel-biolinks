7d0d6e604e2c40b2f92f1f053bc9e9ea41fe1df5
Luciano <lucianoeberhardt95@outlook.com>
2021-12-09 14:33:37 -0300

    schema base para rotas do dashboard

--- routes/dash.php
+++ routes/dash.php
@@ -3,6 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dash', function () {
-    return Inertia::render('Dashboard');
-})->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])
+->group(function() {
+
+    Route::get('/dash', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+
+    
+});
