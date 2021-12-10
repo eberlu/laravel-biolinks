d9689b2d10bf910a666971a1e8eb0358741ae7c3
Luciano <lucianoeberhardt95@outlook.com>
2021-12-10 20:09:32 -0300

    título da página

--- public/js/app.js
+++ public/js/app.js
@@ -25990,7 +25990,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var _hoisted_1 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("h2", {
   "class": "font-semibold text-xl text-gray-800 leading-tight"
-}, " Profile ", -1
+}, " Perfil ", -1
 /* HOISTED */
 );
 
